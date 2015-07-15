f0c5a914a199e009529966f526e0b5d9b4f9c9b7
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-07-15 22:33:54 +0400

    t

--- SharpAlg.Geo/Primitives.cs
+++ SharpAlg.Geo/Primitives.cs
@@ -9,14 +9,14 @@ using NewExpr = SharpAlg.Geo.Core.Expr;
 using Numerics;
 
 namespace SharpAlg.Geo {
-    public class NewPoint {
-        public static NewPoint FromName(char name) {
+    public class Point {
+        public static Point FromName(char name) {
             if(!char.IsUpper(name))
                 throw new InvalidOperationException();
-            return new NewPoint(Param(name + "x"), Param(name + "y"));
+            return new Point(Param(name + "x"), Param(name + "y"));
         }
         public readonly NewExpr X, Y;
-        public NewPoint(NewExpr x, NewExpr y) {
+        public Point(NewExpr x, NewExpr y) {
             this.X = x;
             this.Y = y;
         }
@@ -25,15 +25,15 @@ namespace SharpAlg.Geo {
         }
     }
 
-    public class NewLine {
-        public static NewLine FromPoints(NewPoint p1, NewPoint p2) {
+    public class Line {
+        public static Line FromPoints(Point p1, Point p2) {
             var a = Subtract(p1.Y, p2.Y);
             var b = Subtract(p2.X, p1.X);
             var c = Subtract(Multiply(p1.X, p2.Y), Multiply(p2.X, p1.Y));
-            return new NewLine(a, b, c);
+            return new Line(a, b, c);
         }
         public readonly NewExpr A, B, C;
-        public NewLine(NewExpr a, NewExpr b, NewExpr c) {
+        public Line(NewExpr a, NewExpr b, NewExpr c) {
             this.A = a;
             this.B = b;
             this.C = c;
@@ -43,17 +43,17 @@ namespace SharpAlg.Geo {
         }
     }
 
-    public class NewCircle {
-        public static NewCircle FromPoints(NewPoint p1, NewPoint p2) {
+    public class Circle {
+        public static Circle FromPoints(Point p1, Point p2) {
             var r = Add(
                         Subtract(p1.X, p2.X).Square(),
                         Subtract(p1.Y, p2.Y).Square()
                     );
-            return new NewCircle(p1.X, p1.Y, r);
+            return new Circle(p1.X, p1.Y, r);
         }
         public readonly NewExpr X, Y, R;
-        public NewPoint Center { get { return new NewPoint(X, Y); } }
-        public NewCircle(NewExpr x, NewExpr y, NewExpr r) {
+        public Point Center { get { return new Point(X, Y); } }
+        public Circle(NewExpr x, NewExpr y, NewExpr r) {
             this.X = x;
             this.Y = y;
             this.R = r;
@@ -62,34 +62,34 @@ namespace SharpAlg.Geo {
             return Build((X, Y, R, x, y) => ((x - X) ^ 2) + ((y - Y) ^ 2) - R, X, Y, R, Param("x"), Param("y")).ToString();
         }
     }
-    public static class NewLinesOperations {
-        public static NewPoint Intersect(this NewLine l1, NewLine l2) {
+    public static class LinesOperations {
+        public static Point Intersect(this Line l1, Line l2) {
             var x = Build((A1, B1, C1, A2, B2, C2) => (B1 * C2 - B2 * C1) / (A1 * B2 - A2 * B1), l1.A, l1.B, l1.C, l2.A, l2.B, l2.C);
             var y = Build((A1, B1, C1, A2, B2, C2) => (C1 * A2 - C2 * A1) / (A1 * B2 - A2 * B1), l1.A, l1.B, l1.C, l2.A, l2.B, l2.C);
-            return new NewPoint(x, y);
+            return new Point(x, y);
         }
-        public static NewExpr TangentBetween(NewLine l1, NewLine l2) {
+        public static NewExpr TangentBetween(Line l1, Line l2) {
             return Build((A1, B1, A2, B2) => (A1 * B2 - A2 * B1) / (A1 * A2 + B1 * B2), l1.A, l1.B, l2.A, l2.B);
         }
-        public static NewExpr CotangentBetween(NewLine l1, NewLine l2) {
+        public static NewExpr CotangentBetween(Line l1, Line l2) {
             return Build((A1, B1, A2, B2) => (A1 * A2 + B1 * B2) / (A1 * B2 - A2 * B1), l1.A, l1.B, l2.A, l2.B);
         }
-        public static NewExpr GetY(NewLine l, NewExpr x) {
+        public static NewExpr GetY(Line l, NewExpr x) {
             return Build((A, B, C, X) => -(A * X + C) / B, l.A, l.B, l.C, x);
         }
     }
-    public static class NewLineCircleIntersector {
-        public static System.Tuple<NewPoint, NewPoint> Intersect(this NewLine l, NewCircle c) {
+    public static class LineCircleIntersector {
+        public static System.Tuple<Point, Point> Intersect(this Line l, Circle c) {
             var eqXA = Build((A, B) => (B ^ 2) + (A ^ 2), l.A, l.B);
             var eqXB = Build((A, B, C, X, Y) => 2 * Y * A * B - 2 * X * (B ^ 2) + 2 * C * A, l.A, l.B, l.C, c.X, c.Y);
             var eqXC = Build((A, B, C, X, Y, R) => 2 * Y * B * C + (Y ^ 2) * (B ^ 2) + (C ^ 2) + (X ^ 2) * (B ^ 2) - R * (B ^ 2), l.A, l.B, l.C, c.X, c.Y, c.R);
             var xRoots = QuadraticEquationHelper.Solve(eqXA, eqXB, eqXC);
-            var yRoots = xRoots.FMap(root => NewLinesOperations.GetY(l, root));
-            return Tuple.Create(new NewPoint(xRoots.Item1, yRoots.Item1), new NewPoint(xRoots.Item2, yRoots.Item2));
+            var yRoots = xRoots.FMap(root => LinesOperations.GetY(l, root));
+            return Tuple.Create(new Point(xRoots.Item1, yRoots.Item1), new Point(xRoots.Item2, yRoots.Item2));
         }
     }
-    public static class NewCirclesIntersector {
-        public static System.Tuple<NewPoint, NewPoint> Intersect(this NewCircle c1, NewCircle c2) {
+    public static class CirclesIntersector {
+        public static System.Tuple<Point, Point> Intersect(this Circle c1, Circle c2) {
             var c = c2.Offset(c1.Center.Invert());
             var eqA = Build((X0, Y0) => 4 * (X0 ^ 2) + 4 * (Y0 ^ 2), c.X, c.Y);
             var eqYB = Build((X0, Y0, R1, R2) => -4 * (Y0 ^ 3) - 4 * R1 * Y0 + 4 * Y0 * R2 - 4 * (X0 ^ 2) * Y0, c.X, c.Y, c1.R, c.R);
@@ -99,8 +99,8 @@ namespace SharpAlg.Geo {
             var xRoots = QuadraticEquationHelper.Solve(eqA, eqXB, eqXC);
             var yRoots = QuadraticEquationHelper.Solve(eqA, eqYB, eqYC);
             return Tuple.Create(
-                new NewPoint(xRoots.Item1, yRoots.Item2),
-                new NewPoint(xRoots.Item2, yRoots.Item1)
+                new Point(xRoots.Item1, yRoots.Item2),
+                new Point(xRoots.Item2, yRoots.Item1)
             ).FMap(x => x.Offset(c1.Center));
         }
     }
@@ -115,12 +115,9 @@ namespace SharpAlg.Geo {
     }
 
     public static class Functor {
-        public static NewPoint FMap(this NewPoint x, Func<NewExpr, NewExpr> f) {
-            return new NewPoint(f(x.X), f(x.Y));
+        public static Point FMap(this Point x, Func<NewExpr, NewExpr> f) {
+            return new Point(f(x.X), f(x.Y));
         }
-        //public static NewLine FMap(this NewLine x, Func<NewExpr, NewExpr> f) {
-        //    return new NewLine(f(x.A), f(x.B), f(x.C));
-        //}
         public static System.Tuple<TResult, TResult> FMap<T, TResult>(this System.Tuple<T, T> x, Func<T, TResult> f) {
             return Tuple.Create(f(x.Item1), f(x.Item2));
         }
@@ -130,19 +127,10 @@ namespace SharpAlg.Geo {
         public static NewExpr Square(this NewExpr e) {
             return Core.ExprExtensions.Power(e, 2);
         }
-        public static Expr Multiply(this Expr e, Expr multiplier) {
-            return Expr.Multiply(e, multiplier);
-        }
-        public static Expr Power(this Expr e, Expr power) {
-            return Expr.Power(e, power);
-        }
         public static NewExpr GetHalf(this NewExpr e) {
             return Core.ExprExtensions.Multiply(e, Const(new BigRational(1, 2)));
         }
-        //public static bool IsPrimitive(this Point p) {
-        //    return p.X is ParameterExpr && p.Y is ParameterExpr;
-        //}
-        public static ImmutableContext RegisterPoint(this ImmutableContext context, NewPoint p, double x, double y) {
+        public static ImmutableContext RegisterPoint(this ImmutableContext context, Point p, double x, double y) {
             return context
                 .RegisterValue((Core.ParamExpr)p.X, x)
                 .RegisterValue((Core.ParamExpr)p.Y, y);
@@ -158,30 +146,21 @@ namespace SharpAlg.Geo {
         public static ImmutableContext RegisterValue(this ImmutableContext context, string name, double value) {
             return context.RegisterValue(Expr.Parameter(name), value);
         }
-        public static RealPoint ToRealPoint(this NewPoint p, ImmutableContext context) {
+        public static RealPoint ToRealPoint(this Point p, ImmutableContext context) {
             return new RealPoint(p.X.ToReal(context), p.Y.ToReal(context));
         }
-        public static double ToReal(this Expr expr, ImmutableContext context) {
-            return expr.Evaluate(context).ToDouble();
+        public static Point Offset(this Point p, Point offset) {
+            return new Point(Add(p.X, offset.X), Add(p.Y, offset.Y));
         }
-        public static Expr AsConst(this double value) {
-            return Expr.Constant(value);
+        public static Point Invert(this Point p) {
+            return new Point(Minus(p.X), Minus(p.Y));
         }
-        public static Expr Convolute(this Expr expr) {
-            return expr;
+        public static Point Middle(Point p1, Point p2) {
+            return new Point(Add(p1.X, p2.X).GetHalf(), Add(p1.Y, p2.Y).GetHalf());
         }
-        public static NewPoint Offset(this NewPoint p, NewPoint offset) {
-            return new NewPoint(Add(p.X, offset.X), Add(p.Y, offset.Y));
-        }
-        public static NewPoint Invert(this NewPoint p) {
-            return new NewPoint(Minus(p.X), Minus(p.Y));
-        }
-        public static NewPoint Middle(NewPoint p1, NewPoint p2) {
-            return new NewPoint(Add(p1.X, p2.X).GetHalf(), Add(p1.Y, p2.Y).GetHalf());
-        }
-        public static NewCircle Offset(this NewCircle c, NewPoint offset) {
+        public static Circle Offset(this Circle c, Point offset) {
             var center = c.Center.Offset(offset);
-            return new NewCircle(center.X, center.Y, c.R);
+            return new Circle(center.X, center.Y, c.R);
         }
     }
 }
--- SharpAlg.Geo/Tests/ProblemsTests.cs
+++ SharpAlg.Geo/Tests/ProblemsTests.cs
@@ -22,23 +22,23 @@ namespace SharpAlg.Geo.Tests {
         #region middle
         [Test]
         public void MiddleOfLineSegment_Calc() {
-            var x = NewPoint.FromName('X');
-            var y = NewPoint.FromName('Y');
+            var x = Point.FromName('X');
+            var y = Point.FromName('Y');
             var point = GetMiddleOfLineSegmentZeroAssertion(x, y);
             AssertHelper.ArePointsEqual(new RealPoint(0, 0), point.ToRealPoint(ImmutableContext.Empty.RegisterPoint(x, 1, 2).RegisterPoint(y, 5, 9)));
         }
         string MiddleOfLineSegment_Maple() {
-            var res = GetMiddleOfLineSegmentZeroAssertion(NewPoint.FromName('X'), NewPoint.FromName('Y'));
+            var res = GetMiddleOfLineSegmentZeroAssertion(Point.FromName('X'), Point.FromName('Y'));
             return string.Format("simplify({0}); simplify({1});", res.X, res.Y);
         }
-        NewPoint GetMiddleOfLineSegmentZeroAssertion(NewPoint p1, NewPoint p2) {
-            var l1 = NewLine.FromPoints(p1, p2);
+        Point GetMiddleOfLineSegmentZeroAssertion(Point p1, Point p2) {
+            var l1 = Line.FromPoints(p1, p2);
 
-            var c1 = NewCircle.FromPoints(p1, p2);
-            var c2 = NewCircle.FromPoints(p2, p1);
+            var c1 = Circle.FromPoints(p1, p2);
+            var c2 = Circle.FromPoints(p2, p1);
 
             var c1_c2 = c1.Intersect(c2);
-            var l2 = NewLine.FromPoints(c1_c2.Item1, c1_c2.Item2);
+            var l2 = Line.FromPoints(c1_c2.Item1, c1_c2.Item2);
 
             var l1_l2 = l1.Intersect(l2);
 
@@ -51,62 +51,62 @@ namespace SharpAlg.Geo.Tests {
         #region angle bisector
         [Test]
         public void AngleBisection_Calc() {
-            var a = NewPoint.FromName('A');
-            var b = NewPoint.FromName('B');
-            var c = NewPoint.FromName('C');
+            var a = Point.FromName('A');
+            var b = Point.FromName('B');
+            var c = Point.FromName('C');
             var value = GetAngleBisectionZeroAssertion(a, b, c);
             Assert.AreEqual(0, value.ToReal(ImmutableContext.Empty.RegisterPoint(a, 1, 2).RegisterPoint(b, 5, 9).RegisterPoint(c, 3, 7)), AssertHelper.Delta);
         }
         string AngleBisection_Maple() {
-            var res = GetAngleBisectionZeroAssertion(NewPoint.FromName('A'), NewPoint.FromName('B'), NewPoint.FromName('C'));
+            var res = GetAngleBisectionZeroAssertion(Point.FromName('A'), Point.FromName('B'), Point.FromName('C'));
             //var res = GetAngleBisectionZeroAssertion(new Point(Expr.Zero, Expr.Zero), Point.FromName('B'), Point.FromName('C'));
             return string.Format("simplify({0});", res);
             //Clipboard.SetText(mappleCommand);
         }
-        NewExpr GetAngleBisectionZeroAssertion(NewPoint A, NewPoint B, NewPoint C) {
-            var l1 = NewLine.FromPoints(A, B);
-            var l2 = NewLine.FromPoints(A, C);
+        NewExpr GetAngleBisectionZeroAssertion(Point A, Point B, Point C) {
+            var l1 = Line.FromPoints(A, B);
+            var l2 = Line.FromPoints(A, C);
 
-            var c = NewCircle.FromPoints(A, C);
+            var c = Circle.FromPoints(A, C);
             var c_l2 = l1.Intersect(c).Item1;
 
             var middle = ExprHelper.Middle(C, c_l2);//TODO make real
-            var bisectrissa = NewLine.FromPoints(A, middle);
+            var bisectrissa = Line.FromPoints(A, middle);
 
-            return Add(NewLinesOperations.TangentBetween(l1, bisectrissa), NewLinesOperations.TangentBetween(l2, bisectrissa));
+            return Add(LinesOperations.TangentBetween(l1, bisectrissa), LinesOperations.TangentBetween(l2, bisectrissa));
         }
         #endregion
 
         #region perpendicular
         [Test]
         public void Perpendicular_Calc() {
-            var a = NewPoint.FromName('A');
-            var b = NewPoint.FromName('B');
-            var c = NewPoint.FromName('C');
+            var a = Point.FromName('A');
+            var b = Point.FromName('B');
+            var c = Point.FromName('C');
             var value = GetPerpendicularZeroAssertion(a, b, c);
             Assert.AreEqual(0, value.ToReal(ImmutableContext.Empty.RegisterPoint(a, 1, 2).RegisterPoint(b, 5, 9).RegisterPoint(c, 3, 7)), AssertHelper.Delta);
         }
         string Perpendicular_Maple() {
             //var res = GetPerpendocularZeroAssertion(new Point(Expr.Zero, Expr.Zero), Point.FromName('B'), Point.FromName('C'));
-            var res = GetPerpendicularZeroAssertion(new NewPoint(0, 0), new NewPoint(-1, 0), NewPoint.FromName('C'));
+            var res = GetPerpendicularZeroAssertion(new Point(0, 0), new Point(-1, 0), Point.FromName('C'));
             return string.Format(@"
 assume(Cx>0);
 assume(Cy>0);
 simplify({0});
 ", res);
         }
-        NewExpr GetPerpendicularZeroAssertion(NewPoint A, NewPoint B, NewPoint C) {
-            var l1 = NewLine.FromPoints(A, B);
+        NewExpr GetPerpendicularZeroAssertion(Point A, Point B, Point C) {
+            var l1 = Line.FromPoints(A, B);
 
-            var c = NewCircle.FromPoints(C, A);
+            var c = Circle.FromPoints(C, A);
 
             var D = l1.Intersect(c).Item1; //????
-            var l2 = NewLine.FromPoints(C, D);
+            var l2 = Line.FromPoints(C, D);
             var E = l2.Intersect(c).Item2; //????1
 
-            var l3 = NewLine.FromPoints(E, A);
+            var l3 = Line.FromPoints(E, A);
 
-            var cotangent = NewLinesOperations.CotangentBetween(l1, l3);
+            var cotangent = LinesOperations.CotangentBetween(l1, l3);
             return cotangent;
 
         } 
@@ -115,31 +115,31 @@ simplify({0});
         #region perpendicular
         [Test]
         public void Perpendicular2_Calc() {
-            var res = GetPerpendocularZeroAssertion2(new NewLine(0, 1, 4), new NewCircle(0, 0, 25));
+            var res = GetPerpendocularZeroAssertion2(new Line(0, 1, 4), new Circle(0, 0, 25));
             Assert.AreEqual(0, res.ToReal(ImmutableContext.Empty));
             //Assert.IsTrue(res.ExprEquals(Expr.Zero));
         }
         [Test]
         public void Perpendicular2_Calc2() {
-            var res = GetPerpendocularZeroAssertion2(new NewLine(0, 1, 4), new NewCircle(0, 0, 25));
+            var res = GetPerpendocularZeroAssertion2(new Line(0, 1, 4), new Circle(0, 0, 25));
             Assert.AreEqual(0, res.ToReal(ImmutableContext.Empty));
         }
         string Perpendicular2_Maple() {
             //var res = GetPerpendocularZeroAssertion2(new Line(Expr.Parameter("k"), Expr.One, Expr.Parameter("b")), new Circle(Expr.Zero, Expr.Zero, Expr.Parameter("R")));
-            var res = GetPerpendocularZeroAssertion2(new NewLine(0, 1, Param("b")), new NewCircle(Param("X0"), Param("Y0"), Param("R")));
+            var res = GetPerpendocularZeroAssertion2(new Line(0, 1, Param("b")), new Circle(Param("X0"), Param("Y0"), Param("R")));
             return string.Format(@"
 assume(R>0);
 simplify({0});
 ", res);
         }
-        NewExpr GetPerpendocularZeroAssertion2(NewLine l, NewCircle c) {
+        NewExpr GetPerpendocularZeroAssertion2(Line l, Circle c) {
             var A = l.Intersect(c).Item2;
             var B = l.Intersect(c).Item1;
-            var l2 = NewLine.FromPoints(c.Center, B);
+            var l2 = Line.FromPoints(c.Center, B);
             var C = l2.Intersect(c).Item2;
-            var l3 = NewLine.FromPoints(A, C);
+            var l3 = Line.FromPoints(A, C);
 
-            var cotangent = NewLinesOperations.CotangentBetween(l, l3);
+            var cotangent = LinesOperations.CotangentBetween(l, l3);
             return cotangent;
         }
         #endregion
