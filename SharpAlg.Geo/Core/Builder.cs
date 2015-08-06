5e4c9fee784a2f85e92b37839d854959d137d64f
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-08-07 00:52:27 +0400

    t

--- SharpAlg.Geo/Core/Builder.cs
+++ SharpAlg.Geo/Core/Builder.cs
@@ -6,14 +6,8 @@ namespace SharpAlg.Geo.Core {
         public Expr Add(params Expr[] args) {
             return new AddExpr(this, ImmutableArray.Create(args));
         }
-        public Expr Subtract(Expr a, Expr b) {
-            return Add(a, Minus(b));
-        }
         public Expr Multiply(params Expr[] args) {
             return new MultExpr(this, ImmutableArray.Create(args));
-        }
-        public Expr Minus(Expr a) {
-            return Multiply(-1, a);
         }
         public Expr Divide(Expr a, Expr b) {
             return new DivExpr(this, a, b);
@@ -22,4 +16,5 @@ namespace SharpAlg.Geo.Core {
             return new PowerExpr(this, value, power);
         }
     }
+
 }
