077586812c441e41d3967b0506df708804c13e8a
VitalyTVA <vitalytva@yandex.ru>
2015-08-12 19:07:21 +0300

    t

--- SharpAlg.Geo/Tests/NormalizerTests.cs
+++ SharpAlg.Geo/Tests/NormalizerTests.cs
@@ -10,6 +10,9 @@ namespace SharpAlg.Geo.Tests {
 
             builder.AssertIsNormal(true, x => 2 * x);
             builder.AssertIsNormal(false, x => x * 2);
+            builder.AssertIsNormal(true, (x, y) => x * y);
+            builder.AssertIsNormal(false, (x, y) => x * (y + 1));
+            //builder.AssertIsNormal(false, (x, y) => y * x);
         }
         
     }
