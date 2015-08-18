fd8f3ded5e53748ace58051cd32ac09460031749
Vitaly Tolstikov <vitalytva@yandex.ru>
2015-08-19 01:27:10 +0400

    t

---
+++ SharpAlg.Geo/Core/ImmutableListWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SharpAlg.Geo.Core {
+    public partial struct ImmutableListWrapper<T> : IEnumerable<T> {
+        public static bool operator !=(ImmutableListWrapper<T> left, ImmutableListWrapper<T> right) {
+            return !(left == right);
+        }
+        public static bool operator ==(ImmutableListWrapper<T> left, ImmutableListWrapper<T> right) {
+            return Equals(left.list, right.list);
+        }
+        readonly IList<T> list;
+        public ImmutableListWrapper(IList<T> list) {
+            this.list = list;
+        }
+
+        public override bool Equals(object obj) {
+            if(!(obj is ImmutableListWrapper<T>))
+                return false;
+            return this == (ImmutableListWrapper<T>)obj;
+        }
+
+        public override int GetHashCode() {
+            return list.GetHashCode();
+
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() {
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return list.GetEnumerator();
+        }
+    }
+}
