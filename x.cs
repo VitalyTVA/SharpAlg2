8845e97145a15a1a13b28a2ae1042a6f673675d8
VitalyTVA <vitalytva@yandex.ru>
2015-08-28 18:03:30 +0300

    t

--- x.cs
+++ x.cs
@@ -1,3 +1,5 @@
+[6:01:18 PM] Alex Chuev: https://www.dropbox.com/sh/jjrcmbseo4smpdb/AAAugR-YpdS8jhsMP4EMseVBa?dl=0
+
 SymbolAnalysisContext
 [6:34:30 PM] vladimir mokhovikov:         static INamedTypeSymbol GetDependencyPropertyType(Compilation compilation) {
             return compilation.GetTypeByMetadataName("System.Windows.DependencyProperty");
